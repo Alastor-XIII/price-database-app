05d1493438c636a90f83df2ce16831b7c58cb21f
Alastor-XIII <alastor0104@gmail.com>
2026-03-20 13:01:35 +0700

    Update edit.py

--- pages/edit.py
+++ pages/edit.py
@@ -1,13 +1,22 @@
 import streamlit as st
 import pandas as pd
+import os
 
 st.title("✏️ Edit Product Data")
 
-df = pd.read_csv("data/products.csv")
+file_path = "data/products.csv"
 
+if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+    df = pd.read_csv(file_path)
+else:
+    st.warning("No data found")
+    st.stop()
+
+# แก้ไขตาราง
 edited_df = st.data_editor(df, num_rows="dynamic")
 
+# Save
 if st.button("💾 Save Changes"):
-    edited_df.to_csv("data/products.csv", index=False)
-    st.success("Saved!")
-    st.rerun()
+    edited_df.to_csv(file_path, index=False)
+    st.success("Saved! ✅")
+    st.experimental_rerun()
